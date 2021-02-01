de33bed39a0caacc462b6cefd74a653fa87dd1ca
Sepehr Laal <sepehr.laal@gmail.com>
2021-02-01 15:09:13 -0800

    PoC checked in

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+#!/usr/bin/env node
+
 const _ = require("lodash");
 const fs = require("fs");
 const cp = require("child_process");
 const sct = require("source-code-tokenizer");
 const debug = require("debug")("complexity-js");
 const assert = require("assert");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
 
 // all languages supported by guesslang package
 // can be obtained by executing "npx nopenv guesslang -l"
 // make sure extensions match the list in tokenizer package:
 // https://github.com/devreplay/source-code-tokenizer/blob/master/src/extensionmap.ts
-const extensions = {
+const EXTENSIONS = {
   ".bat": "Batchfile",
   ".c": "C",
   ".cs": "C#",
@@ -42,6 +47,8 @@ const extensions = {
   ".ts": "TypeScript",
 };
 
+const DEFAULT_ARGS = { source: "", file: "", grammar: "" };
+
 async function guessFileSource(src = "") {
   const work = new Promise((resolve, reject) => {
     debug("executing python through nopy to guess language");
@@ -57,11 +64,18 @@ async function guessFileSource(src = "") {
         .toLowerCase();
       debug("guesslang language: %s", language);
       const extension = _.findKey(
-        extensions,
+        EXTENSIONS,
         (lang) => lang.toLowerCase() === language
       );
       debug("guesslang extension: %s", extension);
-      resolve(`source${extension}`);
+      const {
+        grammarPaths,
+      } = require("source-code-tokenizer/lib/extensionmap");
+      const grammarPath = _.findKey(
+        grammarPaths,
+        (g) => _.first(g) === extension
+      );
+      resolve(grammarPath);
     });
     debug("writing the source to stdin: %s", src);
     proc.stdin.write(src);
@@ -72,31 +86,69 @@ async function guessFileSource(src = "") {
   return source;
 }
 
-async function tokenize(args = { source: "", file: "", ext: "" }) {
+async function tokenize(args = DEFAULT_ARGS) {
   assert.ok(args.file || args.source, "provide a file path OR source code");
   assert.ok(!(args.file && args.source), "remove either file OR source code");
   let src = "",
-    ext = args.ext ? `source${args.ext}` : "";
+    grammar = args.grammar ? `source${args.grammar}` : "";
   if (args.file) {
     src = await fs.promises.readFile(args.file, { encoding: "utf-8" });
-    if (!ext) {
-      ext = sct.getFileSource(args.file);
-      ext = ext ? ext : await guessFileSource(src);
+    if (!grammar) {
+      grammar = sct.getFileSource(args.file);
+      grammar = grammar ? grammar : await guessFileSource(src);
     }
   }
   if (args.source) {
     src = args.source;
-    if (!ext) {
-      ext = await guessFileSource(src);
+    if (!grammar) {
+      grammar = await guessFileSource(src);
     }
   }
-  const { tokens } = await sct.tokenize(src, ext);
+  const { tokens } = await sct.tokenize(src, grammar);
   return tokens;
 }
 
+async function extractOperators(args = DEFAULT_ARGS) {
+  const tokens = await tokenize(args);
+  const operators = tokens.filter(
+    (token) =>
+      token.scopes.filter((scope) => scope.includes("operator")).length > 0
+  );
+  return operators.map((op) => op.value);
+}
+
 async function main() {
-  const tokens = await tokenize({ file: "index.js" });
-  debugger;
+  const operators = await extractOperators({
+    file: argv.file || __filename,
+  });
+  const OTO = 1.54; // Operand to Operator constant
+
+  const n1 = _.uniq(operators).length;
+  const n2 = Math.floor(OTO * n1);
+  const N1 = operators.length;
+  const N2 = Math.floor(OTO * N1);
+
+  const programVocabulary = n1 * n2;
+  const programLength = n1 * n2;
+  const calculatedProgramLength = n1 * Math.log2(n1) + n2 * Math.log2(n2);
+  const volume = programLength * Math.log2(programVocabulary);
+  const difficulty = (n1 / 2) * (N2 / n2);
+  const effort = difficulty * volume;
+  const timeToProgram = effort / 18;
+  const deliveredBugs = volume / 3000;
+
+  debug("n1: %s", n1);
+  debug("n2: %s", n2);
+  debug("N1: %s", N1);
+  debug("N2: %s", N2);
+  debug("vocabulary: %s", programVocabulary);
+  debug("length: %s", programLength);
+  debug("calculated_length: %s", calculatedProgramLength);
+  debug("volume: %s", volume);
+  debug("difficulty: %s", difficulty);
+  debug("effort: %s", effort);
+  debug("time: %s", timeToProgram);
+  debug("bugs: %s", deliveredBugs);
 }
 
 main();
